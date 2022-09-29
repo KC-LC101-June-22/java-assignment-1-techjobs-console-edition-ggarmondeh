9dac6abdc13741bcc3472a046909df60ae5d5eb8
Nancy Dolo <nancydolo@Nancys-Air.mshome.net>
2022-09-29 17:04:21 -0500

    your message

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -10,7 +7,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +109,30 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() > 0) {
+            for (HashMap<String, String> someJob : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> column : someJob.entrySet()) {
+                    System.out.println(column.getKey() + ": " + column.getValue());
 
-        System.out.println("printJobs is not implemented yet");
+
+                }
+                System.out.println("*****");
+            }
+
+        } else {
+            System.out.println("No Results");
+        }
     }
 }
+
+
+
+
